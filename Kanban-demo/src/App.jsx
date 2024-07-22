c0c1b1e25cc47a4dc4feda0ddeb005103ac9fe2e
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2024-07-22 20:32:01 +0530

    Basic DND implemented for list-headers

---
+++ Kanban-demo/src/App.jsx
@@ -0,0 +1,101 @@
+import {DragDropContext,Draggable,Droppable} from 'react-beautiful-dnd'
+import { useState } from 'react';
+import './App.css'
+
+const DATA = [
+  {
+    id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
+    name: "Walmart",
+    items: [
+      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
+      { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
+    ],
+    tint: 1,
+  },
+  {
+    id: "487f68b4-1746-438c-920e-d67b7df46247",
+    name: "Indigo",
+    items: [
+      {
+        id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
+        name: "Designing Data Intensive Applications",
+      },
+      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
+    ],
+    tint: 2,
+  },
+  {
+    id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
+    name: "Lowes",
+    items: [
+      { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
+      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
+    ],
+    tint: 3,
+  },
+];
+
+
+function App() {
+
+  const [stores,setStores] = useState(DATA)
+
+  const handleDragDrop = (result) => {
+    const {source,destination,type} = result
+
+    if(!destination) return
+
+    if(source.droppableId === destination.droppableId && source.index === destination.index) return
+
+    if(type === 'group'){
+      const reorderedStores = [...stores]
+
+      const srcIndex = source.index
+      const destIndex = destination.index
+
+      const [removedStore] = reorderedStores.splice(srcIndex,1)
+
+      reorderedStores.splice(destIndex,0,removedStore)
+
+      return setStores(reorderedStores)
+    }
+  }
+    return (
+      <div className='layout__wrapper'>
+        <div className="card">
+
+          <DragDropContext onDragEnd={handleDragDrop}> 
+            <div className="header">
+              <h1>Shopping list</h1>
+            </div>
+
+            <Droppable droppableId='ROOT' type='group'>
+              {(provided)=>(
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                      {
+                        stores.map((store,idx) => (
+                          <Draggable draggableId={store.id} index={idx} key={store.id}>
+                            {(provided)=>(
+                                <div 
+                                  {...provided.dragHandleProps} 
+                                  {...provided.draggableProps} 
+                                  ref={provided.innerRef}
+                                  className='store-container'
+                                >
+                                  <h3>{store.name}</h3>
+                                </div>
+                            )}
+                          </Draggable>
+                        ))
+                      }
+                  </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+      </div>
+    )
+  
+}
+
+export default App
